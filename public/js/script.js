1220c4299f96c9e449a92e8a98de7d3571fd254a
ishwar kawade <kawadeishwar007@gmail.com>
2025-04-28 15:51:39 +0530

    update files

--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,15 @@
       }, false)
     })
   })()
+
+  let taxSwitch = document.getElementById("switchCheckDefault");
+  taxSwitch.addEventListener("click", () =>{
+    let taxinfo = document.getElementsByClassName("tax-info");
+    for(info of taxinfo){
+      if(info.style.display != "inline"){
+      info.style.display = "inline";
+    } else {
+      info.style.display = "none";
+    }
+  }
+  })
